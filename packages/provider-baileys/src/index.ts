d10f5aac1e68ef6b6048ec3566be347ec4830a6f
Alexandre Castillo <31748204+alexandrecastillo@users.noreply.github.com>
2024-10-06 22:25:15 -0500

    Update provider-baileys/index.ts
    add export default from bindStore

--- packages/provider-baileys/src/index.ts
+++ packages/provider-baileys/src/index.ts
@@ -1,6 +1,7 @@
 export * from './bailey'
 export * from './baileyWrapper'
 export * from './bindStore'
+export { default } from './bindStore'
 export * from './releaseTmp'
 export * from './type'
 export * from './utils'
